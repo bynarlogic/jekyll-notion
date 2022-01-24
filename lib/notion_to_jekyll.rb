21a6a06cca4e64ff4909635255be10734b2c86f8
Joshua Jarvis <jjarvis@hey.com>
2022-01-24 08:23:32 -0500

    now loading secrets into root module

--- lib/notion_to_jekyll.rb
+++ lib/notion_to_jekyll.rb
@@ -2,15 +2,19 @@
 
 require "httparty"
 require "require_all"
+require "dotenv"
 
 require_relative "notion_to_jekyll/api"
-require_all "lib/notion_to_jekyll/blocks"
 require_relative "notion_to_jekyll/errors"
 require_relative "notion_to_jekyll/page"
 require_relative "notion_to_jekyll/version"
 
+require_all "lib/notion_to_jekyll/blocks"
+
 # The main entry point for NotionToJekyll
 module NotionToJekyll
+  Dotenv.load("secret.env")
+
   def render_page(url)
     page = Page.new url
 
